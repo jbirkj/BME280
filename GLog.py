75b2b939b0449ef338553c3e03fd8994657d1af9
jbirkj <jesperbirkp@gmail.com>
2015-11-10 15:32:54 +0100

    added httperror exception

--- GLog.py
+++ GLog.py
@@ -1,23 +1,46 @@
 # GLog
+import time,datetime
 import json
 import gspread
 from oauth2client.client import SignedJwtAssertionCredentials
 
 
 def GLog(dT, hP, rH, tC):
-    json_key = json.load(open('Uploader-8ffdcddb7d70.json'))
-    scope = ['https://spreadsheets.google.com/feeds']
 
-    credentials = SignedJwtAssertionCredentials(json_key['client_email'],
-                                             json_key['private_key'].encode(),
-                                             scope )
+    retry = 3
+    while retry > 0:
+    
+        try:
 
-    gc = gspread.authorize(credentials)
-    wks = gc.open("TestOauthSheet").sheet1
+            json_key = json.load(open('Uploader-8ffdcddb7d70.json'))
+            scope = ['https://spreadsheets.google.com/feeds']
 
-    values = [dT, 'BME280', hP, rH, tC]
+            credentials = SignedJwtAssertionCredentials(json_key['client_email'],
+                                                 json_key['private_key'].encode(),
+                                                 scope )
+            #raise  gspread.exceptions.HTTPError()
+        
+            gc = gspread.authorize(credentials)
+            wks = gc.open("TestOauthSheet").sheet1
 
-    wks.append_row(values)
+            values = [dT, 'BME280', hP, rH, tC]
+
+            wks.append_row(values)
+
+            #print( "exception not hit" )
+
+            retry = 0
+            
+
+        except gspread.exceptions.HTTPError:
+            time.sleep(5)
+            print("exception caught no ", retry, " at ", datetime.datetime.now() )
+            retry -= 1
+
+
+
+    
+            
 
     
 
