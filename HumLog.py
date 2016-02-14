3c9873f2994238c3c70aa6faca13a880fd64b8f5
jbirkj <jesperbirkp@gmail.com>
2016-02-14 16:32:45 +0100

    project added DS18B20 logging. Currently BME280 is not enabled

--- HumLog.py
+++ HumLog.py
@@ -6,18 +6,20 @@ from BME280 import BME280read
 
 tNow = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
 t = datetime.datetime.now()
+count = 0
 
 try:
 
     while True:
 
-        t = datetime.datetime.now()
-        
+       
         values = BME280read()
+        
         GLog( t, float(values[0]), float(values[1]), float(values[2])  )
-        #print(values)
-
-        time.sleep(5)
+        
+        print(count, t.hour,':', t.minute, "measure ", float(values[2]) )
+        #time.sleep(60)
+        count = count + 1
 
 except KeyboardInterrupt:
 
